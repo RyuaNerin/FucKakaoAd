fea4e22a532405eef7037fe89d91eaa4a50f7519
RyuaNerin <admin@ryuanerin.kr>
2019-03-28 03:31:34 +0900

    아이콘 추가

--- FucKakaoAd/resource.h
+++ FucKakaoAd/resource.h
@@ -1,18 +1,19 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++¿¡¼­ »ı¼ºÇÑ Æ÷ÇÔ ÆÄÀÏÀÔ´Ï´Ù.
-// FucKakaoAd.rc¿¡¼­ »ç¿ëµÇ°í ÀÖ½À´Ï´Ù.
+// Microsoft Visual C++ì—ì„œ ìƒì„±í•œ í¬í•¨ íŒŒì¼ì…ë‹ˆë‹¤.
+// FucKakaoAd.rcì—ì„œ ì‚¬ìš©ë˜ê³  ìˆìŠµë‹ˆë‹¤.
 //
 #define IDS_STRING_TITLE                101
 #define IDS_STRING_ERROR_NOT_FOUND      102
 #define IDS_STRING_ERROR_PERMISION      103
 #define IDS_STRING_FAIL                 104
 #define IDF_DLL                         105
+#define IDI_ICON1                       107
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40002
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
